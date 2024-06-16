fe4ee169327dc761275b57b579ff304b40f7ee35
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-06-16 11:21:33 +0200

    WrongMovePossible1

--- src/ttfe/tests/SimpleTests.java
+++ src/ttfe/tests/SimpleTests.java
@@ -94,8 +94,8 @@ public class SimpleTests {
 
 	@Test
 	public void testWrongMovepossible1(){
-		for (int i = 1; i < 5; i++) {
-			for (int j = 1; j < 5; j++) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
 				game.setPieceAt(i, j, (i + j) % 2 == 0 ? 2 : 4);
 			}
 		}
