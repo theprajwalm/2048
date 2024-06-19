396625f46d2031a499414776604d0e919749a273
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-06-19 15:32:22 +0200

    dgsdg

---
+++ src/ttfe/SimulatorGame.java
@@ -0,0 +1,83 @@
+package ttfe;
+
+public class SimulatorGame implements SimulatorInterface {
+
+    @Override
+    public void addPiece() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPiece'");
+    }
+
+    @Override
+    public int getBoardHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardHeight'");
+    }
+
+    @Override
+    public int getBoardWidth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardWidth'");
+    }
+
+    @Override
+    public int getNumMoves() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNumMoves'");
+    }
+
+    @Override
+    public int getNumPieces() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNumPieces'");
+    }
+
+    @Override
+    public int getPieceAt(int x, int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPieceAt'");
+    }
+
+    @Override
+    public int getPoints() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPoints'");
+    }
+
+    @Override
+    public boolean isMovePossible() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMovePossible'");
+    }
+
+    @Override
+    public boolean isMovePossible(MoveDirection direction) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMovePossible'");
+    }
+
+    @Override
+    public boolean isSpaceLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isSpaceLeft'");
+    }
+
+    @Override
+    public boolean performMove(MoveDirection direction) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'performMove'");
+    }
+
+    @Override
+    public void run(PlayerInterface player, UserInterface ui) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    @Override
+    public void setPieceAt(int x, int y, int piece) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPieceAt'");
+    }
+    
+}
